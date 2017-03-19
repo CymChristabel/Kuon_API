7abe076a44db54afa4cc83c35ce34b25337a54c7
cym.christable <cym.christable@gmail.com>
2017-03-19 23:56:05 +0800

    add study time synchronize

--- api/controllers/StudyTimeStatisticsController.js
+++ api/controllers/StudyTimeStatisticsController.js
@@ -1,11 +1,12 @@
-import * as _ from 'lodash';
+var _ = require('lodash');
+var moment = require('moment');
 
 module.exports = {
 
 	find(req, res){
 		if(req.param('date'))
 		{
-			StudyTimeStatistics.find({ user: req.param('user'), date: req.param('date') }).exec((err, result) => {
+			StudyTimeStatistics.find({ user: req.param('user'), date: req.param('date'), sort: 'date ASC' }).exec((err, result) => {
 				if(err)
 				{
 					console.log(err);
@@ -16,7 +17,7 @@ module.exports = {
 		}
 		else
 		{
-			StudyTimeStatistics.find({ user: req.param('user') }).exec((err, result) => {
+			StudyTimeStatistics.find({ user: req.param('user'), sort: 'date ASC' }).exec((err, result) => {
 				if(err)
 				{
 					console.log(err);
@@ -27,35 +28,64 @@ module.exports = {
 		}
 	},
 
-	createOrUpdate: (req, res) => {
-		StudyTimeStatistics.find({ user: req.param('user'), date: req.param('date') }).exec((err, result) => {
-			if(err)
-			{
+	createOrUpdate(req, res){
+		let data = req.param('data');
+		console.log(data);
+		StudyTimeStatistics.find({ user: data.user, date: data.date }).exec((err, result) =>{
+			if(err){
 				console.log(err);
 				return res.serverError(err);
 			}
 			if(result.length == 0)
 			{
-				StudyTimeStatistics.create(req.allParams()).exec((err, data) => {
-					if(err)
-					{
-						console.log(err);
-						return res.serverError(err);
+				StudyTimeStatistics.create(data).exec((finalErr, finalResult) => {
+					if(finalErr){
+						console.log(finalErr);
+						return res.serverError(finalErr);
 					}
-					return res.json(_.omit(data, ['createdAt', 'updatedAt', 'deletedAt']));
+					return res.json(finalResult);
 				});
 			}
 			else
 			{
-				StudyTimeStatistics.update({ user: req.param('user'), date: req.param('date') }, { nceTime: req.param('nceTime'), recitationTime: req.param('recitationTime') }).exec((err, data) => {
-					if(err)
-					{
-						console.log(err);
-						return res.serverError(err);
+				if(req.param('hasSynchronized') == false)
+				{
+					data.nceTime = data.nceTime + result[0].nceTime;
+					data.recitationTime = data.recitationTime + result[0].recitationTime;
+				}
+				StudyTimeStatistics.update({ user: data.user, date: data.date }, { nceTime: data.nceTime, recitationTime: data.recitationTime }).exec((finalErr, finalResult) => {
+					if(finalErr){
+						console.log(finalErr);
+						return res.serverError(finalErr);
 					}
-					return res.json(_.omit(data, ['createdAt', 'updatedAt', 'deletedAt']));
-				})
+					return res.json(finalResult);
+				});
 			}
+		});
+	},
+
+	synchronize: (req, res) => {
+		let temp = req.param('data');
+		let userID = req.param('user');
+		let data = [];
+
+		for(let key in temp)
+		{
+			data.push({
+				date: key,
+				nceTime: temp[key].nceTime,
+				recitationTime: temp[key].recitationTime,
+				user: userID
+			})
+		}
+
+		StudyTimeStatistics.findOrCreate(data).exec((err, result) => {
+			if(err)
+			{
+				console.log(err);
+				return res.serverError(err);
+			}
+			return res.json(result);
 		})
 	}
 	
